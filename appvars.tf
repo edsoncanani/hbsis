5e33e3ce511573b959ae381dd56b8885da123352
edsoncanani <68755419+edsoncanani@users.noreply.github.com>
2020-07-28 01:13:44 -0300

    Add files via upload

---
+++ appvars.tf
@@ -0,0 +1,19 @@
+# Nome da Aplicacao
+variable "app_name" {
+  type        = string
+}
+
+#Ambiente
+variable "environment" {
+  type        = string
+}
+
+#Local
+variable "location" {
+  type        = string
+}
+
+#Descricao
+variable "description" {
+  type        = string
+}
